d2c9d4851cc9a32c6462d63810c67082a0917a83
Ankur Pandey <49517709+ankurpandey27@users.noreply.github.com>
2025-09-04 18:42:33 +0530

    Time: 53 ms (54.25%), Space: 59.7 MB (17.16%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,15 +10,29 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// var hasCycle = function(head) {
+//     let visitedNode = new Set();
+//     let curr = head;
+//     while(curr){
+//         if(visitedNode.has(curr)){
+//             return true
+//         }
+//         visitedNode.add(curr)
+//         curr = curr.next;
+//     }
+//     return false;
+// };
+
 var hasCycle = function(head) {
-    let visitedNode = new Set();
-    let curr = head;
-    while(curr){
-        if(visitedNode.has(curr)){
-            return true
+    if(!head) return false;
+    let slow = head;
+    let fast = head.next; 
+    while(slow != fast){
+        if(!fast || !fast.next){
+            return false
         }
-        visitedNode.add(curr)
-        curr = curr.next;
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    return false;
+    return true;
 };
